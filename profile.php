db400c1fb77e9517fb1f3148fa754963263c93fc
Ahmed Hassan <43516324+ahmedhassan20029@users.noreply.github.com>
2022-03-19 11:24:18 -0700

    update

---
+++ profile.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+if(isset($_SESSION['user'])) {
+
+    $titlePage = "profile";
+    include "front-pages/init_front.php";
+
+    $getuser = $con->prepare('SELECT * FROM users WHERE UserName = ?');
+    $getuser->execute(array($_SESSION['user']));
+    $info = $getuser->fetch();
+?>
+    <div class='container'>
+        <div>
+            <?php
+                echo $info['UserName'] . '<br>';
+
+                foreach(getitems('Member_ID', $info['UserID']) as $rows) {
+                    echo $rows['Name'] . '<br>';
+                }
+
+            ?>
+        </div>
+
+    </div>
+<?php
+    include "front-pages/" . $dir_templates . 'footer.php';
+
+} else {
+
+    header('location: login.php?action=login');
+    exit();
+
+}
